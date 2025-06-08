71624c8da8c4e808312bf2b90c7df89027ad973a
kanishkaprism <kanishka.thakur@prism.training>
2025-06-08 10:40:22 +0100

    Implemented basic http server

---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	w.Write([]byte("Hello from Snippetbox!"))
+}
+
+func viewSnippet(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprintf(w, "View a specific Snippet with id %d...", id)
+}
+
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		w.Header().Set("Allow", http.MethodPost)
+		http.Error(w, "Only POST method is allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	w.Write([]byte("Create a Snippet!"))
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", viewSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
+
+	log.Print("Starting server at port 4000..")
+
+	err := http.ListenAndServe(":4000", mux)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
