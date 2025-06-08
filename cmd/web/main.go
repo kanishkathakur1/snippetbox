7f1e0192aca76830da6b64c9820d18f97eae0328
kanishkaprism <kanishka.thakur@prism.training>
2025-06-08 18:40:25 +0100

    added flags and logging

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,13 +1,26 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+	// Using flags for configuring HTTP address
+	addr := flag.String("addr", ":4000", "HTTP network address")
+
+	flag.Parse()
+
+	// Separate loggers for info and errors
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	// fileserver for static files
 	fileserver := http.FileServer(http.Dir("./ui/static/"))
 
+	// A new mux for hadling requests
 	mux := http.NewServeMux()
 
 	mux.Handle("/static/", http.StripPrefix("/static", fileserver))
@@ -16,7 +29,7 @@ func main() {
 	mux.HandleFunc("/snippet/view", viewSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
 
-	log.Print("Starting server at port :4000...")
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
+	infoLog.Printf("Starting server at port %s", *addr)
+	err := http.ListenAndServe(*addr, mux)
+	errorLog.Fatal(err)
 }
